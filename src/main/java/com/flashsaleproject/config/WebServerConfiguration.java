e4919bf4547c8f0c7cf11689583bcdabfcb1bdb8
yuzhencode <yuzhen@Yuzhens-MacBook-Pro.local>
2024-07-04 18:30:56 +0100

    change localhost config

---
+++ src/main/java/com/flashsaleproject/config/WebServerConfiguration.java
@@ -0,0 +1,37 @@
+package com.flashsaleproject.config;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+
+//当Spring容器内没有TomcatEmbeddedServletContainerFactory这个bean时，会吧此bean加载进spring中
+@Component
+public class WebServerConfiguration implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        //Customise our tomcat connector with the interfaces provided by the corresponding factory class.
+        ((TomcatServletWebServerFactory)factory).addConnectorCustomizers(new TomcatConnectorCustomizer() {
+            @Override
+            public void customize(Connector connector) {
+                Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+
+                //Customise keepalivetimeout, set the server to automatically disconnect
+                // the keepalive link if there is no request within 30 seconds.
+                protocol.setKeepAliveTimeout(30000);
+                //Automatically breaks the keepalive link when the client sends more than
+                // 10,000 requests
+                protocol.setMaxKeepAliveRequests(10000);
+            }
+        });
+
+
+
+
+    }
+}
