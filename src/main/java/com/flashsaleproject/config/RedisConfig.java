d9597ba4ed24cc0ca992bd43f91c86665bea20ec
yuzhencode <yuzhen@Yuzhens-MacBook-Pro.local>
2024-07-11 17:33:12 +0100

    In a distributed session, token-based verification is used to check the user's login status.

--- src/main/java/com/flashsaleproject/config/RedisConfig.java
+++ src/main/java/com/flashsaleproject/config/RedisConfig.java
@@ -1,10 +1,10 @@
-//package com.flashsaleproject.config;
-//
-//
-//import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600)
-//public class RedisConfig {
-//}
+package com.flashsaleproject.config;
+
+
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600)
+public class RedisConfig {
+}
