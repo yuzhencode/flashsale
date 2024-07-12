8ab068ca4c4ea9f5e90cff2391607d84c5c6cf5d
yuzhencode <yuzhen@Yuzhens-MacBook-Pro.local>
2024-07-12 15:23:38 +0100

    Redis Centralised Cache

---
+++ src/main/java/com/flashsaleproject/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.flashsaleproject.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:html/static/img");
+
+    }
+}
